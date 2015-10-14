c5f2ed8c319b58692974cf45eb287e07dcebffe0
Martbov <Mart@RN-145-97-165-197.eduroam.rug.nl>
2015-10-14 10:27:21 +0200

    Modified test.py

--- test.py
+++ test.py
@@ -1,13 +1,35 @@
 #usr/bin/python3.4
 
+from progressbar import ProgressBar
+
 def main():
 	dutchfile = open('valuelookup.txt', 'r')
 	englishfile = open('valuereference.txt', 'r')
 
-	dutchlines = dutchfile.readlines()
-	englishlines = englishfile.readlines()
+	dutchlines = []
+	englishlines = []
 
-	print(dutchlines)
+	for line in dutchfile:
+		dutchlines.append(line.strip().split())
+	for line in englishfile:
+		englishlines.append(line.strip().split())
 
+	propertyList = []
+	propertySet = set()
+	pbar = ProgressBar()
+	for linelistD in pbar(dutchlines):
+		for linelistE in englishlines:
+			if linelistD[0] == linelistE[0]:
+				if linelistD[2] == linelistE[2]:
+					value = '{:<60} dbpedia-owl:sameAs {}'.format(linelistD[1], linelistE[1])
+					#print(value)
+					#print('{:<60} dbpedia-owl:sameAs {}'.format(linelistD[1], linelistE[1]))
+					#if value not in propertyList:
+					#	propertyList.append(value)
+					propertySet.add(value)
+	#print(propertyList)
+	print(propertySet)
+
+	
 if __name__ == '__main__':
 	main()
